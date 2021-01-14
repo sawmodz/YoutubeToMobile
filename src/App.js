ef1d528b4a1510fcd64756697ea7de354c713a62
BlizzFull <41762589+sawmodz@users.noreply.github.com>
2021-01-14 18:58:23 +0100

    The main file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+/*global chrome*/
+import React from 'react'
+import './App.css'
+const QRCode = require('qrcode.react')
+let currentSecond = '0'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Notfound extends React.PureComponent {
+  state={
+    currentWindows:"Current",
+    currentTime:'0'
+  }
+  componentDidMount(){
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+      let url = tabs[0].url
+      let tab = tabs[0]
+      this.setState({currentWindows:url})
+      chrome.tabs.executeScript(tab.id, {
+        code: 'document.querySelector(".ytp-time-current").textContent'
+      }, (results)=>{
+        if(this.state.currentWindows.includes('youtube')){
+          let h1=results +""
+          let secondes = "0"
+          if(h1.split(':')[1] == '01' || h1.split(':')[1] == '02' || h1.split(':')[1] == '03'|| h1.split(':')[1] == '04'|| h1.split(':')[1] == '05'|| h1.split(':')[1] == '06' || h1.split(':')[1] == '08' || h1.split(':')[1] == '09'){
+            secondes = h1.split(':')[1].replace('0','')
+          }else if (h1.split(':')[1] == '00'){
+            secondes = "0"
+          }else{
+            secondes = h1.split(':')[1]
+          }
+          let seconde = parseInt(h1.split(':')[0])*60 + parseInt(secondes)
+          this.setState({currentTime:('&t='+seconde)})
+          document.querySelector("#id1").innerHTML = "<p>Titre : " + tab.title + "</p><p>Time : " + h1.split(':')[0] + " minutes, " + secondes + " secondes" + "</p><a target='_blank' rel='noopener noreferrer' href='"+this.state.currentWindows + this.state.currentTime+"'>Lien : Clique ici" + "</a>" ;
+        }else{
+          this.setState({currentTime:''})
+        }
+      })
+    })
+  }
+  render () {
+    return (
+      <div>
+        <h1 className='Titre'>QRCODE</h1>
+        <QRCode className='qrcode' value={this.state.currentWindows + this.state.currentTime} />
+        <div id="id1">Isn't Youtube page</div>
+      </div>
+    )
+  }
 }
 
-export default App;
